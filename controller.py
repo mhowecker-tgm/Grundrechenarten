15452bdc97d0827f47b11b46fb2aefc5867d3d2d
Martin Howecker <mhowecker@student.tgm.ac.at>
2022-09-21 16:43:56 +0200

    code kommentiert

--- controller.py
+++ controller.py
@@ -6,18 +6,21 @@ import view
 
 class Controller:
     def __init__(self):
+        # Model und View als Variablen anlegen
         self.model = model.Grundrechnung()
         self.view = view.View(self)
 
     def reset(self) -> None:
-        print("here")
+        # Model und View zurücksetzen
         self.model.reset()
         self.view.reset()
 
     def execute(self) -> None:
+        # Eingaben lesen
         op1 = self.view.get_op1()
         op2 = self.view.get_op2()
         operator = self.view.get_operator()
+        # Ergebnis berechnen und ausgeben
         error = self.model.berechnen(op1, op2, operator)
         t = "Die Berechnung war erfolgreich!"
         if error is None:
--- view.py
+++ view.py
@@ -5,6 +5,8 @@ from controller import Controller
 
 class View(QMainWindow):
 
+    # Klassenvariablen für die Autovervollständigung
+
     coB_operator: QComboBox
     pb_execute: QPushButton
     pb_reset: QPushButton
@@ -14,17 +16,23 @@ class View(QMainWindow):
     statusbar: QStatusBar
 
     def __init__(self, c: Controller):
+        # super const. immer aufrufen
         super().__init__()
+        # GUI laden
         uic.loadUi("Grundrechenarten.ui", self)
+        # ComboBox mit Operatoren füllen
         self.coB_operator.addItem("+")
         self.coB_operator.addItem("-")
         self.coB_operator.addItem("*")
         self.coB_operator.addItem("/")
+        # Handler für die Buttons verbinden
         self.pb_execute.clicked.connect(c.execute)
         self.pb_reset.clicked.connect(c.reset)
 
     def reset(self) -> None:
+        # oberstes Element der ComboBox auswählen
         self.coB_operator.setCurrentIndex(0)
+        # Operanden auf 0 setzen
         self.spB_operand1.setValue(0)
         self.spB_operand2.setValue(0)
         self.l_act_result.setText("Noch kein Ergebnis")
@@ -38,7 +46,6 @@ class View(QMainWindow):
         self.statusbar.showMessage(t)
 
     def get_op1(self) -> int:
-        self.spB_operand1.value()
         return self.spB_operand1.value()
 
     def get_op2(self) -> int:
